d3d511c608a3d80c2bd09432d33d6a5527f3d34a
Thirumalaivasan-Nagarajan <thirumalaivasann.dev@gmail.com>
2026-02-12 09:30:54 +0530

    Remove default tests and add money tests

---
+++ jasmine/moneyTest.js
@@ -0,0 +1,18 @@
+import {priceFormatting} from "../scripts/utils/money.js";
+
+describe("Test suite:PriceFormatting!!!",()=>
+{
+  it("Convert cents into currency",()=>
+  {
+    expect(priceFormatting(2095)).toEqual('20.95');
+  });
+
+  it("Convert zero into currency",()=>
+  {
+    expect(priceFormatting(0)).toEqual('0.00');
+  });
+  it("Rounds to nearest cent",()=>
+  {
+    expect(priceFormatting(2000.5)).toEqual('20.01');
+  });
+});
